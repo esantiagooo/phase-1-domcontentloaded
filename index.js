7374e9b856574ef2813d9c0554173a862cb6e1e3
Eric <eric.yanez50@qmail.cuny.edu>
2022-02-11 11:10:25 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    changeDom()
+})
+
+const changeDom = function(){
+    document.getElementById("text")
+    .innerHTML = "This is really cool!"
+}
